6c27797e7ba73b8204b57df86e87117a72cadc45
IbnuAhsani <ibnu.muhari@gmail.com>
2018-02-28 23:35:32 +0700

    Add shelter DB & pets Table

---
+++ app/src/main/java/com/example/android/pets/data/PetContract.java
@@ -0,0 +1,35 @@
+package com.example.android.pets.data;
+
+import android.provider.BaseColumns;
+
+/**
+ * Created by test-pc on 27-Feb-18.
+ */
+
+public class PetContract {
+
+    private PetContract(){};
+
+    public static abstract class PetsEntry implements BaseColumns
+        {
+             /* Defining the name of the Column Heading of the Table */
+            public static final String _ID = BaseColumns._ID;
+            public static final String TABLE_PET_NAME = "pet";
+            public static final String COLUMN_PET_NAME = "name";
+            public static final String COLUMN_PET_BREED = "breed";
+            public static final String COLUMN_PET_GENDER = "gender";
+            public static final String COLUMN_PET_WEIGHT = "weight";
+
+            public static final String TEXT_TYPE = "TEXT";
+            public static final String INTEGER_TYPE = "INTEGER";
+            public static final String PRIMARY_KEY = "PRIMARY KEY";
+            public static final String AUTOINCREMENT = "AUTOINCREMENT";
+            public static final String NOT_NULL = "NOT NULL";
+            public static final String DEFAULT = "DEFAULT";
+
+            /* Possible values for Gender of Pets */
+            public static final int GENDER_MALE = 1;
+            public static final int GENDER_FEMALE = 2;
+            public static final int GENDER_UNKNOWN = 0;
+        }
+}
